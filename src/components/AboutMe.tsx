0403cd35618ae8d77fa361d1ddc3cb5cdf7b18b4
ndemeyvan <ndemeyvan@gmail.com>
2022-09-07 19:33:27 +0000

    finish with general content

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MdKeyboardTab ,MdOutlineNavigateNext} from "react-icons/md";
+import { MdKeyboardTab, MdOutlineNavigateNext } from "react-icons/md";
 import Avatar from "../assets/avatar.png";
 
 const AboutMe = () => {
@@ -7,11 +7,19 @@ const AboutMe = () => {
     <div className=" md:h-[80vh] mx-auto ">
       <div className="flex md:flex-row h-full items-center justify-center ">
         {/* image */}
-        <div className=" h-[600px] w-[400px] bg-no-repeat bg-center">
+        <div
+          className=" h-[600px] w-[400px] bg-no-repeat bg-center "
+          data-aos="fade-right"
+          data-aos-delay="400"
+        >
           <img src={Avatar} alt="" />
         </div>
         {/* left side about me */}
-        <div className="ml-[50px] text-semibold">
+        <div
+          className="ml-[50px] text-semibold"
+          data-aos="fade-left"
+          data-aos-delay="400"
+        >
           <h1 className="h1 text-7xl font-bold md:mb-[50px]">About Me</h1>
           <p className="max-w-[600px] md:mb-[25px]">
             Always looking forward to joining a progressive team. Self-taught, I
@@ -24,7 +32,11 @@ const AboutMe = () => {
             Contact me for more information, I would be honored to read you very
             soon.
           </p>
-          <div className="mt-[25px] bg-white w-[200px]   py-4 text-black font-bold border-2 border-black rounded-none hover:bg-blue-500 hover:text-white transition  ">
+          <div
+            className="mt-[25px] bg-white w-[200px]   py-4 text-black font-bold border-2 border-black rounded-none hover:bg-blue-500 hover:text-white transition"
+            data-aos="fade-left"
+            data-aos-delay="600"
+          >
             <a
               href="https://www.linkedin.com/in/ndemeyvan/"
               target="_blank"
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -10,6 +10,7 @@ import {
   SiJavascript,
   SiTypescript,
   SiJava,
+  SiKotlin,
 } from "react-icons/si";
 import { AiFillGithub } from "react-icons/ai";
 import { DiDart } from "react-icons/di";
@@ -29,7 +30,8 @@ const Skills = () => {
         {/* Section thinks */}
         <div className="grid grid-cols-3 w-full">
           {/* Coding tools */}
-          <div className="px-[50px] p-[30px] hover:bg-green-500 hover:shadow-lg   hover:text-white transition duration-600 text-black">
+          <div className="px-[50px] p-[30px] hover:bg-green-500 hover:shadow-lg   hover:text-white transition duration-600 text-black"  data-aos="fade-down"
+            data-aos-delay="200">
             <h3 className="text-center font-bold text-3xl">
               DEVELOPMENT TOOLS
             </h3>
@@ -49,21 +51,23 @@ const Skills = () => {
                 <SiAndroidstudio className=" transition ease-in-out duration-500 text-blue-900 text-6xl my-[15px]" />
                 <p className="text-justify">
                   The best text editor in the world for mobile application
-                  development.
+                  development. I use it to create Flutter or Native applications
+                  with Kotlin
                 </p>
               </div>
               {/* GitHub */}
               <div className="flex flex-col items-center mt-[30px] ">
                 <AiFillGithub className=" transition ease-in-out duration-500 text-black text-6xl my-[15px] " />
                 <p className="text-justify">
-                  The best text editor in the world for mobile application
-                  development.
+                  Git is the code versioning tool I use regularly to version my
+                  code, you can find all my public projects on my github.
                 </p>
               </div>
             </div>
           </div>
           {/* Framwork */}
-          <div className="px-[50px] p-[30px]  hover:bg-green-500 hover:shadow-lg   hover:text-white transition duration-600 text-black">
+          <div className="px-[50px] p-[30px]  hover:bg-green-500 hover:shadow-lg   hover:text-white transition duration-600 text-black" data-aos="fade-down"
+            data-aos-delay="300">
             <h3 className="text-center font-bold text-3xl">FRAMEWORK</h3>
             <hr className="mt-[15px]" />
             {/* Elements */}
@@ -73,37 +77,47 @@ const Skills = () => {
               <div className="flex flex-col items-center mt-[30px]">
                 <SiFlutter className=" transition ease-in-out duration-500 text-blue-500 text-6xl my-[15px]" />
                 <p className="text-justify">
-                  The best text editor in the world for web development.
+                  Flutter is undoubtedly one of my favorite frameworks, after
+                  several years of native development, the discovery of Flutter
+                  was a great surprise for me, anyone who has ever had to make a
+                  recyclerview against a listview will understand what I mean
+                  :).
                 </p>
               </div>
               {/* Tailwind */}
               <div className="flex flex-col items-center mt-[30px]">
                 <SiTailwindcss className=" transition ease-in-out duration-500 text-blue-500 text-6xl my-[15px]" />
                 <p className="text-justify">
-                  The best text editor in the world for mobile application
-                  development.
+                  Without a doubt the best approach to css franwork,
+                  minimalized, intuitive, fun, I completely fell in love with
+                  this framework that has a totally different approach to the
+                  styling of an html page.
                 </p>
               </div>
               {/* Nestjs */}
               <div className="flex flex-col items-center mt-[30px]">
                 <SiNestjs className=" transition ease-in-out duration-500 text-red-500 text-6xl my-[15px]" />
                 <p className="text-justify">
-                  The best text editor in the world for mobile application
-                  development.
+                  Easy to use, flexible and complete, Nestjs is the framework
+                  that made me become a FullStack developer. More orderly and
+                  clean than a simple project created just with Express.
                 </p>
               </div>
               {/* Reactjs */}
               <div className="flex flex-col items-center mt-[30px]">
                 <FaReact className=" transition ease-in-out duration-500 text-blue-500 text-6xl my-[15px]" />
                 <p className="text-justify">
-                  The best text editor in the world for mobile application
-                  development.
+                  Reactjs is my favorite, after having experienced other js
+                  frameworks, like Vuejs or Angular, the simple approach of
+                  Reactjs won me over, creating react components is a real
+                  pleasure
                 </p>
               </div>
             </div>
           </div>
           {/* Cloud  */}
-          <div className="px-[50px] p-[30px]  hover:bg-green-500 hover:shadow-lg   hover:text-white transition duration-600 text-black">
+          <div className="px-[50px] p-[30px]  hover:bg-green-500 hover:shadow-lg   hover:text-white transition duration-600 text-black" data-aos="fade-down"
+            data-aos-delay="450">
             <h3 className="text-center font-bold text-3xl">CLOUD</h3>
             <hr className="mt-[15px]" />
             {/* Elements */}
@@ -113,29 +127,34 @@ const Skills = () => {
               <div className="flex flex-col items-center mt-[30px]">
                 <FaDigitalOcean className=" transition ease-in-out duration-500 text-blue-500 text-6xl my-[15px]" />
                 <p className="text-justify">
-                  The best text editor in the world for web development.
+                  Maybe the best cloud provider for developers and beginners in
+                  the cloud domain. I use it a lot for personal or small
+                  projects.
                 </p>
               </div>
               {/* Aws */}
               <div className="flex flex-col items-center mt-[30px]">
                 <FaAws className=" transition ease-in-out duration-500 text-yellow-500 text-6xl my-[15px]" />
                 <p className="text-justify">
-                  The best text editor in the world for mobile application
-                  development.
+                  More complex to handle than DO, but also more complete and
+                  more expensive. Aws is a cloud provider that I explore from
+                  time to time as needed for specific services like Amazon S3
                 </p>
               </div>
               {/* Azure */}
               <div className="flex flex-col items-center mt-[30px] ">
                 <SiMicrosoftazure className=" transition ease-in-out duration-500 text-blue-500 text-6xl my-[15px]" />
                 <p className="text-justify">
-                  The best text editor in the world for mobile application
-                  development.
+                  More complex to handle than DO, but also more complete and
+                  more expensive. Aws is a cloud provider that I explore from
+                  time to time as needed for specific services like Azure Blob
                 </p>
               </div>
             </div>
           </div>
           {/* Language  */}
-          <div className="px-[50px] p-[30px]  hover:bg-green-500 hover:shadow-lg   hover:text-white transition duration-600 text-black">
+          <div className="px-[50px] p-[30px]  hover:bg-green-500 hover:shadow-lg   hover:text-white transition duration-600 text-black" data-aos="fade-down"
+            data-aos-delay="4600">
             <h3 className="text-center font-bold text-3xl">LANGUAGES</h3>
             <hr className="mt-[15px]" />
             {/* Elements */}
@@ -145,37 +164,48 @@ const Skills = () => {
               <div className="flex flex-col items-center mt-[30px]">
                 <DiDart className=" transition ease-in-out duration-500 text-blue-500 text-6xl my-[15px]" />
                 <p className="text-justify">
-                  The best text editor in the world for web development.
+                  Dart is a programming language optimized for applications on
+                  multiple platforms. It is developed by Google and is used to
+                  create mobile applications Flutter
                 </p>
               </div>
               {/* JavaScript */}
               <div className="flex flex-col items-center mt-[30px]">
-                <SiJavascript className=" transition ease-in-out duration-500 text-yellow-400 text-6xl my-[15px]" />
+                <SiKotlin className=" transition ease-in-out duration-500 text-red-400 text-6xl my-[15px]" />
                 <p className="text-justify">
-                  The best text editor in the world for mobile application
-                  development.
+                  Kotlin is an object-oriented and functional programming
+                  language with static typing that allows compiling for the Java
+                  virtual machine, JavaScript, and to several platforms
+                  natively.
                 </p>
               </div>
               {/* TypeScript */}
               <div className="flex flex-col items-center mt-[30px] ">
                 <SiTypescript className=" transition ease-in-out duration-500 text-blue-500 text-6xl my-[15px]" />
                 <p className="text-justify">
-                  The best text editor in the world for mobile application
-                  development.
+                  TypeScript is a free and open source programming language
+                  developed by Microsoft that aims to improve and secure the
+                  production of JavaScript code. It is a strict syntax superset
+                  of JavaScript.
                 </p>
               </div>
               {/* Java */}
               <div className="flex flex-col items-center mt-[30px] ">
                 <SiJava className=" transition ease-in-out duration-500 text-red-500 text-6xl my-[15px]" />
                 <p className="text-justify">
-                  The best text editor in the world for mobile application
-                  development.
+                  Java is an object-oriented programming language created by
+                  James Gosling and Patrick Naughton, employees of Sun
+                  Microsystems, with the support of Bill Joy, officially
+                  presented on May 23, 1995 at the .SunWorld The company Sun was
+                  then acquired in 2009 by the company Oracle which now owns and
+                  maintains Java.
                 </p>
               </div>
             </div>
           </div>
           {/* Dev Ops  */}
-          <div className="px-[50px] p-[30px]  hover:bg-green-500 hover:shadow-lg   hover:text-white transition duration-600 text-black">
+          <div className="px-[50px] p-[30px]  hover:bg-green-500 hover:shadow-lg   hover:text-white transition duration-600 text-black" data-aos="fade-down"
+            data-aos-delay="440">
             <h3 className="text-center font-bold text-3xl">Dev Ops / CI-CD</h3>
             <hr className="mt-[15px]" />
             {/* Elements */}
@@ -185,15 +215,18 @@ const Skills = () => {
               <div className="flex flex-col items-center mt-[30px]">
                 <SiGithubactions className=" transition ease-in-out duration-500 text-blue-500 text-6xl my-[15px]" />
                 <p className="text-justify">
-                  The best text editor in the world for mobile application
-                  development.
+                  GitHub Actions is a tool I use to facilitate the automation of
+                  my software workflows, in order to set up Ci/CD pipelines in a
+                  very simple and fast way
                 </p>
               </div>
               {/* Jenkins */}
               <div className="flex flex-col items-center mt-[30px]">
                 <FaJenkins className=" transition ease-in-out duration-500 text-blue-900 text-6xl my-[15px]" />
                 <p className="text-justify">
-                  The best text editor in the world for web development.
+                  Surely the tool of CI/CD the most used to automate the tasks,
+                  I have a rather good comprehension of the operation of this
+                  tool which has already made largely its proof
                 </p>
               </div>
             </div>
